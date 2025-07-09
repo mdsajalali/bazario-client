f41aaab2f0e4a1b07e3e73b9c7afd64d13ac14ff
mdsajalali <mdsajalali2020@gmail.com>
2025-07-09 22:37:33 +0600

    added: dashboard routing

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,12 +1,40 @@
 import { Routes } from '@angular/router';
 import { LoginComponent } from './pages/login/login.component';
 import { RegisterComponent } from './pages/register/register.component';
+import { LayoutComponent } from './pages/admin/layout/layout.component';
+import { DashboardComponent } from './pages/admin/dashboard/dashboard.component';
+import { BrandsComponent } from './pages/admin/brands/brands.component';
+import { CategoriesComponent } from './pages/admin/categories/categories.component';
+import { ProductsComponent } from './pages/admin/products/products.component';
+import { HomeComponent } from './pages/home/home.component';
 
 export const routes: Routes = [
   {
     path: '',
-    redirectTo: 'login',
+    component: HomeComponent,
     pathMatch: 'full',
+  },
+  {
+    path: '',
+    component: LayoutComponent,
+    children: [
+      {
+        path: 'dashboard',
+        component: DashboardComponent,
+      },
+      {
+        path: 'dashboard/brands',
+        component: BrandsComponent,
+      },
+      {
+        path: 'dashboard/categories',
+        component: CategoriesComponent,
+      },
+      {
+        path: 'dashboard/products',
+        component: ProductsComponent,
+      },
+    ],
   },
   {
     path: 'login',
