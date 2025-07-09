14b86e3a93fabf46e1e31a870e6c576a8f53c0dc
mdsajalali <mdsajalali2020@gmail.com>
2025-07-09 21:52:27 +0600

    added: login ui

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -1,11 +1,42 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
+import { IconFieldModule } from 'primeng/iconfield';
+import { CardModule } from 'primeng/card';
+import { ButtonModule } from 'primeng/button';
+import {
+  FormBuilder,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
 
 @Component({
   selector: 'app-login',
-  imports: [],
+  imports: [
+    ReactiveFormsModule,
+    IconFieldModule,
+    RouterLink,
+    CardModule,
+    ButtonModule,
+  ],
   templateUrl: './login.component.html',
-  styleUrl: './login.component.scss'
+  styleUrl: './login.component.scss',
 })
 export class LoginComponent {
+  loginForm!: FormGroup;
 
+  constructor(private fb: FormBuilder) {
+    this.loginForm = this.fb.group({
+      email: ['', [Validators.email, Validators.required]],
+      password: ['', [Validators.required, Validators.minLength(8)]],
+    });
+  }
+
+  onSubmit() {
+    if (this.loginForm.valid) {
+      console.log('Login', this.loginForm.value);
+    } else {
+      this.loginForm.markAsTouched();
+    }
+  }
 }
