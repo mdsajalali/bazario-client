12c0a6fe266cbebd483fe314e155ff0a389c7c50
mdsajalali <mdsajalali2020@gmail.com>
2025-07-10 14:03:40 +0600

    added: update category form ui

--- src/app/pages/admin/categories/categories.component.ts
+++ src/app/pages/admin/categories/categories.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { PaginationComponent } from "../../../components/shared/pagination/pagination.component";
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-categories',
-  imports: [PaginationComponent],
+  imports: [PaginationComponent, RouterLink],
   templateUrl: './categories.component.html',
   styleUrl: './categories.component.scss'
 })
