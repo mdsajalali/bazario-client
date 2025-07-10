905b80257336aa20807e539dc5d9f226bdacc36a
mdsajalali <mdsajalali2020@gmail.com>
2025-07-10 13:46:18 +0600

    added: update brand form ui

--- src/app/pages/admin/brands/brands.component.ts
+++ src/app/pages/admin/brands/brands.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { PaginationComponent } from "../../../components/shared/pagination/pagination.component";
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-brands',
-  imports: [PaginationComponent],
+  imports: [PaginationComponent, RouterLink],
   templateUrl: './brands.component.html',
   styleUrl: './brands.component.scss'
 })
