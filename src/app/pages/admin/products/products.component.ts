33e6cd594ed9c55e60b531d7e225fd350d4ffe58
mdsajalali <mdsajalali2020@gmail.com>
2025-07-10 12:32:30 +0600

    added: pagination ui

--- src/app/pages/admin/products/products.component.ts
+++ src/app/pages/admin/products/products.component.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import { PaginationComponent } from "../../../components/shared/pagination/pagination.component";
 
 @Component({
   selector: 'app-products',
-  imports: [],
+  imports: [PaginationComponent],
   templateUrl: './products.component.html',
   styleUrl: './products.component.scss'
 })
