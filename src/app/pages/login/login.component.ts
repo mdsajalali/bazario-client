c92e279293f34155be717345d5035b1ee7aa9158
mdsajalali <mdsajalali2020@gmail.com>
2025-07-11 22:25:53 +0600

    added: primeng form

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -1,33 +1,61 @@
-import { Component } from '@angular/core';
-import { RouterLink } from '@angular/router';
+import { Component, inject } from '@angular/core';
+import { InputTextModule } from 'primeng/inputtext';
 import {
   FormBuilder,
   FormGroup,
   ReactiveFormsModule,
   Validators,
 } from '@angular/forms';
+import { ButtonModule } from 'primeng/button';
+import { MessageModule } from 'primeng/message';
+import { RouterLink } from '@angular/router';
+import { ToastModule } from 'primeng/toast';
+import { MessageService } from 'primeng/api';
 
 @Component({
   selector: 'app-login',
-  imports: [ReactiveFormsModule, RouterLink],
+  imports: [
+    ReactiveFormsModule,
+    InputTextModule,
+    ButtonModule,
+    MessageModule,
+    RouterLink,
+    ToastModule,
+    MessageModule,
+  ],
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss',
 })
 export class LoginComponent {
-  loginForm!: FormGroup;
+  messageService = inject(MessageService);
+
+  loginForm: FormGroup;
+
+  formSubmitted = false;
 
   constructor(private fb: FormBuilder) {
     this.loginForm = this.fb.group({
-      email: ['', [Validators.email, Validators.required]],
-      password: ['', [Validators.required, Validators.minLength(8)]],
+      username: ['', Validators.required],
+      email: ['', [Validators.required, Validators.email]],
     });
   }
 
   onSubmit() {
+    this.formSubmitted = true;
     if (this.loginForm.valid) {
-      console.log('Login', this.loginForm.value);
-    } else {
-      this.loginForm.markAsTouched();
+      this.messageService.add({
+        severity: 'success',
+        summary: 'Success',
+        detail: 'Form Submitted',
+        life: 3000,
+      });
+      this.loginForm.reset();
+      this.formSubmitted = false;
     }
+  }
+
+  isInvalid(controlName: string) {
+    const control = this.loginForm.get(controlName);
+    return control?.invalid && (control.touched || this.formSubmitted);
   }
 }
