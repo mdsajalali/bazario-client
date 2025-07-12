0464baf2f6e20bc3dec064ad9346041751df475c
mdsajalali <mdsajalali2020@gmail.com>
2025-07-12 17:49:36 +0600

    added: primeng brand table

--- src/app/pages/admin/brands/brands.component.ts
+++ src/app/pages/admin/brands/brands.component.ts
@@ -1,13 +1,62 @@
 import { Component } from '@angular/core';
-import { PaginationComponent } from "../../../components/shared/pagination/pagination.component";
+import { TableModule } from 'primeng/table';
+import { HttpClientModule } from '@angular/common/http';
+import { InputTextModule } from 'primeng/inputtext';
+import { IconField } from 'primeng/iconfield';
+import { InputIcon } from 'primeng/inputicon';
+import { SelectModule } from 'primeng/select';
+import { FormsModule } from '@angular/forms';
 import { RouterLink } from '@angular/router';
+
+interface Brand {
+  brandId: string;
+  name: string;
+  date: string;
+}
 
 @Component({
   selector: 'app-brands',
-  imports: [PaginationComponent, RouterLink],
+  imports: [
+    TableModule,
+    HttpClientModule,
+    InputTextModule,
+    IconField,
+    InputIcon,
+    SelectModule,
+    FormsModule,
+    RouterLink,
+  ],
   templateUrl: './brands.component.html',
-  styleUrl: './brands.component.scss'
+  styleUrl: './brands.component.scss',
 })
 export class BrandsComponent {
+  orders!: Brand[];
 
+  globalFilterValue: string = '';
+
+  ngOnInit() {
+    this.getBrands().then((data) => (this.orders = data));
+  }
+
+  getBrands(): Promise<Brand[]> {
+    return Promise.resolve([
+      {
+        brandId: '#ORD001',
+        name: 'John Doe',
+        date: '2025-07-10',
+      },
+      {
+        brandId: '#ORD001',
+        name: 'Sajal',
+        date: '2025-07-10',
+      },
+    ]);
+  }
+
+  getGlobalFilterValue(filter: any): string {
+    if (filter && !Array.isArray(filter)) {
+      return filter.value || '';
+    }
+    return '';
+  }
 }
