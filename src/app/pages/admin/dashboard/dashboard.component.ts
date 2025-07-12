be6a68143249920c200dabcf8056e98445f73cb0
mdsajalali <mdsajalali2020@gmail.com>
2025-07-12 19:59:12 +0600

    added: dashboard charts

--- src/app/pages/admin/dashboard/dashboard.component.ts
+++ src/app/pages/admin/dashboard/dashboard.component.ts
@@ -1,12 +1,95 @@
-import { Component } from '@angular/core';
-import { OrdersComponent } from "../orders/orders.component";
+import {
+  ChangeDetectorRef,
+  Component,
+  inject,
+  PLATFORM_ID,
+} from '@angular/core';
+import { OrdersComponent } from '../orders/orders.component';
+import { ChartModule } from 'primeng/chart';
+import { isPlatformBrowser } from '@angular/common';
 
 @Component({
   selector: 'app-dashboard',
-  imports: [OrdersComponent],
+  imports: [OrdersComponent, ChartModule],
   templateUrl: './dashboard.component.html',
-  styleUrl: './dashboard.component.scss'
+  styleUrl: './dashboard.component.scss',
 })
 export class DashboardComponent {
+  basicData: any;
+  basicOptions: any;
 
+  platformId = inject(PLATFORM_ID);
+
+  constructor(private cd: ChangeDetectorRef) {}
+
+  ngOnInit() {
+    this.initChart();
+  }
+
+  initChart() {
+    if (isPlatformBrowser(this.platformId)) {
+      const documentStyle = getComputedStyle(document.documentElement);
+      const textColor =
+        documentStyle.getPropertyValue('--p-text-color') || '#495057';
+      const textColorSecondary =
+        documentStyle.getPropertyValue('--p-text-muted-color') || '#6c757d';
+      const surfaceBorder =
+        documentStyle.getPropertyValue('--p-content-border-color') || '#dee2e6';
+
+      this.basicData = {
+        labels: ['Q1', 'Q2', 'Q3', 'Q4'],
+        datasets: [
+          {
+            label: 'Sales',
+            data: [8500, 7200, 9100, 10600],
+            backgroundColor: [
+              'rgba(255, 99, 132, 0.2)',
+              'rgba(54, 162, 235, 0.2)',
+              'rgba(255, 206, 86, 0.2)',
+              'rgba(75, 192, 192, 0.2)',
+            ],
+            borderColor: [
+              'rgba(255, 99, 132, 1)',
+              'rgba(54, 162, 235, 1)',
+              'rgba(255, 206, 86, 1)',
+              'rgba(75, 192, 192, 1)',
+            ],
+            borderWidth: 2,
+          },
+        ],
+      };
+
+      this.basicOptions = {
+        responsive: true,
+        plugins: {
+          legend: {
+            labels: {
+              color: textColor,
+            },
+          },
+        },
+        scales: {
+          x: {
+            ticks: {
+              color: textColorSecondary,
+            },
+            grid: {
+              color: surfaceBorder,
+            },
+          },
+          y: {
+            beginAtZero: true,
+            ticks: {
+              color: textColorSecondary,
+            },
+            grid: {
+              color: surfaceBorder,
+            },
+          },
+        },
+      };
+
+      this.cd.markForCheck();
+    }
+  }
 }
