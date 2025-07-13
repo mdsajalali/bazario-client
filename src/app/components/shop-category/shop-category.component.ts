739efbc4febe93dc2d48bc088fa4d3ca894d5ca1
mdsajalali <mdsajalali2020@gmail.com>
2025-07-13 10:26:20 +0600

    added: shop category ui

---
+++ src/app/components/shop-category/shop-category.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-shop-category',
+  imports: [],
+  templateUrl: './shop-category.component.html',
+  styleUrl: './shop-category.component.scss'
+})
+export class ShopCategoryComponent {
+
+}
--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -2,10 +2,11 @@ import { Component } from '@angular/core';
 import { HeaderComponent } from "../../components/shared/header/header.component";
 import { FooterComponent } from "../../components/shared/footer/footer.component";
 import { BannerComponent } from "../../components/banner/banner.component";
+import { ShopCategoryComponent } from "../../components/shop-category/shop-category.component";
 
 @Component({
   selector: 'app-home',
-  imports: [HeaderComponent, FooterComponent, BannerComponent],
+  imports: [HeaderComponent, FooterComponent, BannerComponent, ShopCategoryComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
