bce31673a3350d737a0f5f261d978add3d66a384
mdsajalali <mdsajalali2020@gmail.com>
2025-07-13 10:07:42 +0600

    added: banner

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { HeaderComponent } from "../../components/shared/header/header.component";
 import { FooterComponent } from "../../components/shared/footer/footer.component";
+import { BannerComponent } from "../../components/banner/banner.component";
 
 @Component({
   selector: 'app-home',
-  imports: [HeaderComponent, FooterComponent],
+  imports: [HeaderComponent, FooterComponent, BannerComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
