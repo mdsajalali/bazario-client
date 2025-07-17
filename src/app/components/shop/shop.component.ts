7804ea382be8ed87ffa84987667131b29b310a12
mdsajalali <mdsajalali2020@gmail.com>
2025-07-17 23:02:21 +0600

    added: shop ui

--- src/app/components/shop/shop.component.ts
+++ src/app/components/shop/shop.component.ts
@@ -1,13 +1,32 @@
-import { Component } from '@angular/core';
-import { HeaderComponent } from "../shared/header/header.component";
-import { FooterComponent } from "../shared/footer/footer.component";
+import { Component, inject } from '@angular/core';
+import { HeaderComponent } from '../shared/header/header.component';
+import { FooterComponent } from '../shared/footer/footer.component';
+import { ProductType } from '../../types';
+import { ProductsService } from '../../services/products/products.service';
+import { ProductCardComponent } from "../shared/product-card/product-card.component";
 
 @Component({
   selector: 'app-shop',
-  imports: [HeaderComponent, FooterComponent],
+  imports: [HeaderComponent, FooterComponent, ProductCardComponent],
   templateUrl: './shop.component.html',
-  styleUrl: './shop.component.scss'
+  styleUrl: './shop.component.scss',
 })
 export class ShopComponent {
+  products: ProductType[] = [];
+  productService = inject(ProductsService);
 
+  ngOnInit() {
+    this.getProducts();
+  }
+
+  getProducts() {
+    this.productService.getProducts().subscribe({
+      next: (result: any) => {
+        this.products = result.products;
+      },
+      error: (error) => {
+        console.log(error);
+      },
+    });
+  }
 }
