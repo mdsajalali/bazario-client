205b54d129fb032f1c41bc2f4ea55bf2f800d605
mdsajalali <mdsajalali2020@gmail.com>
2025-07-19 20:00:18 +0600

    fixed: cors error

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,4 @@
 export const environment = {
-  // apiUrl: 'https://bazario-api.vercel.app/api/v1',
-  apiUrl: 'http://localhost:5000/api/v1',
-
+  apiUrl: 'https://bazario-api.vercel.app/api/v1',
+  // apiUrl: 'http://localhost:5000/api/v1',
 };
