309b9b663f3ec365b98be74457749eaaa63172a4
mdsajalali <mdsajalali2020@gmail.com>
2025-08-07 18:34:13 +0600

    added: blogs fetch form api

--- src/app/components/home-blogs/home-blogs.component.ts
+++ src/app/components/home-blogs/home-blogs.component.ts
@@ -1,10 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { RouterLink } from '@angular/router';
+import { BlogsService } from '../../services/blogs/blogs.service';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-home-blogs',
-  imports: [RouterLink],
+  imports: [RouterLink, CommonModule],
   templateUrl: './home-blogs.component.html',
   styleUrl: './home-blogs.component.scss',
 })
-export class HomeBlogsComponent {}
+export class HomeBlogsComponent {
+  blogsService = inject(BlogsService);
+  blogs: any = [];
+  loading: boolean = true;
+
+  ngOnInit() {
+    this.loading = true;
+    this.blogsService.getBlogs().subscribe({
+      next: (result: any) => {
+        this.blogs = result;
+        this.loading = false;
+      },
+      error: (error) => {
+        console.log(error);
+        this.loading = false;
+      },
+    });
+  }
+}
---
+++ src/app/services/blogs/blogs.service.ts
@@ -0,0 +1,14 @@
+import { HttpClient } from '@angular/common/http';
+import { inject, Injectable } from '@angular/core';
+import { environment } from '../../../environments/environment';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class BlogsService {
+  http = inject(HttpClient);
+
+  getBlogs() {
+    return this.http.get(environment.apiUrl + '/blogs');
+  }
+}
